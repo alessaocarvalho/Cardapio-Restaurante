bc7728175c06bfe5cff1e8db702fbf7a0ebc9c2e
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-02-04 18:54:55 -0300

    Finalização

--- projeto-cardapio/src/App.jsx
+++ projeto-cardapio/src/App.jsx
@@ -1,12 +1,29 @@
+import { useState } from 'react'
 import Capa from './assets/hashtaurante.webp'
 import './App.css'
 import { Navegacao } from './Navegacao'
+import { pratosPrincipais, sobremesas, bebidas } from './cardapio'
 import { ItemCardapio } from './ItemCardapio'
 
 export function App() {
+
+  const paginasMenu = [
+    pratosPrincipais,
+    sobremesas,
+    bebidas
+  ]
+  const [paginaSelecionada, atualizarPaginaSelecinada] = useState(0)
+
   return <>
-    <img src={Capa} alt="" className='capa'/>
-    <Navegacao />
-    <ItemCardapio />
+    <img src={Capa} alt="" className="capa" />
+    <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecinada}/>
+    <div className="menu">
+      {paginasMenu[paginaSelecionada].map(item => <ItemCardapio
+        nome={item.nome}
+        descricao={item.descricao}
+        preco={item.preco}
+        imagem={item.imagem}
+      />)}
+    </div>
   </>
 }
